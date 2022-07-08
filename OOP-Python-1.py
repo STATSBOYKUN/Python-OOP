93200e59f54fe05caf8ebd6d10adff4eb210eb9d
STATSBOYKUN <radenumarhp277@student.uns.ac.id>
2022-07-08 22:19:30 +0700

    Selesai pengenalan python class

--- OOP-Python-1.py
+++ OOP-Python-1.py
@@ -9,7 +9,19 @@ class Character:
       self.hitpoint = data[1]
       self.mana = data[2]
       Character.jumlah += 1
-
+   
+   #void method
+   def say(self):
+      print("Hai! Boku wa "+ self.name)
+   
+   #argument method
+   def tambahMana(self,tambahanMana):
+      self.mana += tambahanMana
+   
+   #method return
+   def jumlahHitpoint(self):
+      return self.hitpoint
+       
 hero1 = Character("Umar", 1000, 500)
 hero2 = Character("Nana", 800, 600)
 hero3 = Character("Violet", 700, 400)
@@ -19,3 +31,4 @@ print(hero2.__dict__)
 print(hero3.__dict__)
 
 print(f"Jumlah character adalah {Character.jumlah}")
+print(f"HP hero1 adalah {hero1.jumlahHitpoint()}")
